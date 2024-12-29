0da74916b51a87543915eb3cbec984ba9e10aa13
Turk2003 <rengar03842@gmail.com>
2024-12-29 10:03:31 +0700

    เพิ่มตัวแปรy

--- C/Lab07/Lab07_01/Lab07_01/Lab07_01/Lab07_01.cpp
+++ C/Lab07/Lab07_01/Lab07_01/Lab07_01/Lab07_01.cpp
@@ -9,7 +9,17 @@ int main() {
     cout << "ค่าผ่าน ptr: " << *ptr << endl;   // เข้าถึงค่าผ่าน ptr
     cout << "ตำแหน่งใน ptr: " << ptr << endl;  // แสดง address ที่ ptr เก็บไว้
 
+
+    int y = 20;
+    ptr = &y; // เก็บ address ของ x
+
+    cout << "ค่าของ y: " << x << endl;           // แสดงค่าของ x
+    cout << "ตำแหน่งของ y: " << &x << endl;    // แสดง address ของ x
+    cout << "ค่าผ่าน ptr: " << *ptr << endl;   // เข้าถึงค่าผ่าน ptr
+    cout << "ตำแหน่งใน ptr: " << ptr << endl;  // แสดง address ที่ ptr เก็บไว้
+
     return 0;
 }
+
 
 
